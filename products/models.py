7c579639277679fa4d6b3bda0a8ec915cfeb77e1
Shane-Bath <>
2023-10-08 12:24:46 +0000

    Edit setting to fix media sync issues with S3 and made changes to image in product model

--- products/models.py
+++ products/models.py
@@ -37,7 +37,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
