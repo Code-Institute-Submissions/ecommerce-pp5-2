72822942892bef436dde1d2a213f425de6d566af
Shane-Bath <>
2023-10-08 11:06:06 +0000

    Add models to admin and completed first product upload.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Catergory, Country, Region
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Catergory)
+admin.site.register(Country)
+admin.site.register(Region)
