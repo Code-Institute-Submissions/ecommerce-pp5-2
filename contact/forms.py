6526556b5951d4fc58d6cace72d8b704757f827d
Shane-Bath <>
2023-12-05 22:48:26 +0000

    Add Recaptcha to my contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Contact
+# from captcha.fields import ReCaptchaField
+# from captcha.widgets import ReCaptchaV2Checkbox
+from django_recaptcha.fields import ReCaptchaField
 
 
 class ContactForm(forms.ModelForm):
@@ -7,6 +10,7 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'subject', 'body',]
+        captcha = ReCaptchaField()
 
         labels = {
             'name': 'Name',
