c5f4e060d44bf157afe99fcd64e3842318e2e260
Shane-Bath <>
2023-12-09 11:39:09 +0000

    Add email address to contact form view

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,5 @@
 from django import forms
 from .models import Contact
-# from captcha.fields import ReCaptchaField
-# from captcha.widgets import ReCaptchaV2Checkbox
-# from django_recaptcha.fields import ReCaptchaField
 
 
 class ContactForm(forms.ModelForm):
@@ -10,7 +7,6 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'subject', 'body',]
-        # captcha = ReCaptchaField()
 
         labels = {
             'name': 'Name',
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -10,7 +13,17 @@ def contact_form(request):
     if request.method == 'POST':
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
-            contact_form.save()
+            # Get the form data
+            name = contact_form.cleaned_data['name']
+            email = contact_form.cleaned_data['email']
+            subject = contact_form.cleaned_data['subject']
+            message = contact_form.cleaned_data['body']
+
+            # Send the email
+            send_mail(subject, message, email, [settings.EMAIL_HOST_USER],)
+
+            # Send a success message
+            messages.info(request, 'Message sent')
             return redirect('products')
     else:
         contact_form = ContactForm()
