a7331491178d1d734b36846fac404db763475672
Shane-Bath <>
2023-12-09 14:50:23 +0000

    Add email to site owner to inform them that a contact email has been sent

--- contact/views.py
+++ contact/views.py
@@ -9,18 +9,33 @@ from django.template.loader import render_to_string
 
 
 def contact_form(request):
+    '''A contact form for the custemor to contact the business.
+       An email is sent to the site owner to notify them that a customer has sent
+       contact email. And an email is sent to the client to confirm reciept of email'''
+
     contact_form = ContactForm()
 
     if request.method == 'POST':
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
-            ''' Get the form data'''
+            # Get the form data
             name = contact_form.cleaned_data['name']
             email = contact_form.cleaned_data['email']
             subject = contact_form.cleaned_data['subject']
             message = contact_form.cleaned_data['body']
 
-            ''' Send the user a confirmation email'''
+            # Send site owner an email
+            site_subject = f'New email from {name}'
+            site_message = f'Subject: {subject}\nFrom: {name} <{email}>\n\n{message}'
+
+            send_mail(
+                site_subject,
+                site_message,
+                settings.EMAIL_HOST_USER,
+                [settings.EMAIL_HOST_USER],
+                )
+
+            # Send the user a confirmation email
 
             subject = render_to_string(
                 'contact_emails/email_confirmation_subject.txt', {'name': name})
